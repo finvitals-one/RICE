5e81ab83b4e597bd01cd7672b88622178749d81b
drnisheeth77 <drnisheeth77@gmail.com>
2026-03-03 15:42:40 +0530

    Reset DB schema

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import os
 import asyncio
 import sqlite3
+import os
+if os.path.exists("rice.db"):
+    os.remove("rice.db")
 from datetime import datetime
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message, CallbackQuery
